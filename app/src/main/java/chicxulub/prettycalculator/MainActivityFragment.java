8da1acfd87f89dca5c3ea90d9a5e7af8b569402f
Emily Bartman <bartman@nau.edu>
2015-09-22 19:39:35 -0700

    Added symbols +,/,*,- functionality

--- app/src/main/java/chicxulub/prettycalculator/MainActivityFragment.java
+++ app/src/main/java/chicxulub/prettycalculator/MainActivityFragment.java
@@ -17,7 +17,8 @@ import java.util.ArrayList;
  */
 public class MainActivityFragment extends Fragment implements View.OnClickListener {
 
-    public TextView outputAlpha, outputBeta;
+    public TextView outputAlpha;
+    public TextView outputBeta;
     public static final String TAG = MainActivityFragment.class.getName();
 
     public MainActivityFragment() {
@@ -32,6 +33,8 @@ public class MainActivityFragment extends Fragment implements View.OnClickListen
         ArrayList<Button> numberButtons = getAllNumberButtons(rootView);
         int i = 0;
         outputAlpha = (TextView)rootView.findViewById(R.id.calculatorOutputBottom);
+        outputBeta = (TextView)rootView.findViewById(R.id.calculatorOutputTop);
+
         for(Button button: numberButtons) {
             // add button onclick listeners
             addNumPadClickListener(button, i, outputAlpha);
@@ -41,9 +44,17 @@ public class MainActivityFragment extends Fragment implements View.OnClickListen
         // add click events for operations
         Button b = (Button)rootView.findViewById(R.id.backspace);
         Button c = (Button)rootView.findViewById(R.id.clear);
+        Button a = (Button)rootView.findViewById(R.id.add);
+        Button s = (Button)rootView.findViewById(R.id.subtract);
+        Button m = (Button)rootView.findViewById(R.id.multiply);
+        Button d = (Button)rootView.findViewById(R.id.divide);
 
-        c.setOnClickListener(this);
         b.setOnClickListener(this);
+        c.setOnClickListener(this);
+        a.setOnClickListener(this);
+        s.setOnClickListener(this);
+        m.setOnClickListener(this);
+        d.setOnClickListener(this);
 
         return rootView;
     }
@@ -70,20 +81,155 @@ public class MainActivityFragment extends Fragment implements View.OnClickListen
     }
 
     public void onClick(View v){
-        String text = outputAlpha.getText().toString();
+        String textAlpha = outputAlpha.getText().toString();
+        String textBeta = outputBeta.getText().toString();
+        String newText = "";
         switch(v.getId()) {
             case R.id.backspace:
-                if(text.length() > 0) {
-                    outputAlpha.setText(text.substring(0, text.length() - 1));
+                if(textAlpha.length()>0) {
+                    outputAlpha.setText(textAlpha.substring(0, textAlpha.length() - 1));
+                } else {
+                    // start deleting in beta
+                    outputBeta.setText(textBeta.substring(0, textBeta.length() - 1));
                 }
                 break;
             case R.id.clear:
-                if(text.length() > 0) {
+                if(textAlpha.length()==0) {
+                    outputBeta.setText("");
+                }
+                outputAlpha.setText("");
+                break;
+            case R.id.add:
+                if(textAlpha.length()>0) {
+                    if (textBeta.length() > 0) {
+                        newText = textBeta;
+                    }
+                    newText += textAlpha + "+";
+                    outputBeta.setText(newText);
+                    outputAlpha.setText("");
+                }
+                break;
+            case R.id.subtract:
+                if(textAlpha.length()>0) {
+                    if (textBeta.length() > 0) {
+                        newText = textBeta;
+                    }
+                    newText += textAlpha + "-";
+                    outputBeta.setText(newText);
+                    outputAlpha.setText("");
+                }
+                break;
+            case R.id.multiply:
+                if(textAlpha.length()>0) {
+                    if (textBeta.length() > 0) {
+                        newText = textBeta;
+                    }
+                    newText += textAlpha + "*";
+                    outputBeta.setText(newText);
+                    outputAlpha.setText("");
+                }
+                break;
+            case R.id.divide:
+                if(textAlpha.length()>0) {
+                    if (textBeta.length() > 0) {
+                        newText = textBeta;
+                    }
+                    newText += textAlpha + "/";
+                    outputBeta.setText(newText);
                     outputAlpha.setText("");
                 }
                 break;
         }
     }
 
+    public static double eval(final String str) {
+        class Parser {
+            int pos = -1, c;
 
+            void eatChar() {
+                c = (++pos < str.length()) ? str.charAt(pos) : -1;
+            }
+
+            void eatSpace() {
+                while (Character.isWhitespace(c)) eatChar();
+            }
+
+            double parse() {
+                eatChar();
+                double v = parseExpression();
+                if (c != -1) throw new RuntimeException("Unexpected: " + (char)c);
+                return v;
+            }
+
+            // Grammar:
+            // expression = term | expression `+` term | expression `-` term
+            // term = factor | term `*` factor | term `/` factor | term brackets
+            // factor = brackets | number | factor `^` factor
+            // brackets = `(` expression `)`
+
+            double parseExpression() {
+                double v = parseTerm();
+                for (;;) {
+                    eatSpace();
+                    if (c == '+') { // addition
+                        eatChar();
+                        v += parseTerm();
+                    } else if (c == '-') { // subtraction
+                        eatChar();
+                        v -= parseTerm();
+                    } else {
+                        return v;
+                    }
+                }
+            }
+
+            double parseTerm() {
+                double v = parseFactor();
+                for (;;) {
+                    eatSpace();
+                    if (c == '/') { // division
+                        eatChar();
+                        v /= parseFactor();
+                    } else if (c == '*' || c == '(') { // multiplication
+                        if (c == '*') eatChar();
+                        v *= parseFactor();
+                    } else {
+                        return v;
+                    }
+                }
+            }
+
+            double parseFactor() {
+                double v;
+                boolean negate = false;
+                eatSpace();
+                if (c == '+' || c == '-') { // unary plus & minus
+                    negate = c == '-';
+                    eatChar();
+                    eatSpace();
+                }
+                if (c == '(') { // brackets
+                    eatChar();
+                    v = parseExpression();
+                    if (c == ')') eatChar();
+                } else { // numbers
+                    StringBuilder sb = new StringBuilder();
+                    while ((c >= '0' && c <= '9') || c == '.') {
+                        sb.append((char)c);
+                        eatChar();
+                    }
+                    if (sb.length() == 0) throw new RuntimeException("Unexpected: " + (char)c);
+                    v = Double.parseDouble(sb.toString());
+                }
+                eatSpace();
+                if (c == '^') { // exponentiation
+                    eatChar();
+                    v = Math.pow(v, parseFactor());
+                }
+                if (negate) v = -v; // unary minus is applied after exponentiation; e.g. -3^2=-9
+                return v;
+            }
+        }
+        return new Parser().parse();
+    }
 }
